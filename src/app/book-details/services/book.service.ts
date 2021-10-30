9d3ed63a58b55021287233856a9f24f790efa343
Nami2012 <namithasadeeshbabu@gmail.com>
2021-10-31 01:22:47 +0530

    Add BookDetailComponent

--- src/app/book-details/services/book.service.ts
+++ src/app/book-details/services/book.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../model/book.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  private REST_API_URL = 'https://mocki.io/v1/4b716538-3493-4f4a-be02-b90e28cae2e2';
+  constructor(private http:HttpClient) {
 
-  constructor() { }
+    
+   }
+   getBookByCategory():Observable<Book[]>{
+    return this.http.get(this.REST_API_URL)
+     .pipe(map((res:any)=>{
+       console.log(res);
+       return res;
+     }));
+   }
 }
