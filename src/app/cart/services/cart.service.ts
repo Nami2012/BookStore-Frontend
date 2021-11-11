2a89f85cb0c69a3634183260ddb29a1e3a95ccc0
Nami2012 <namithasadeeshbabu@gmail.com>
2021-11-11 11:06:49 +0530

    Update cart badge to display total items dynamically

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Cart } from '../model/cart.model';
 import { CartItem } from '../model/cartItem.model';
+import { take } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   private cart: Cart = new Cart();
+  private cartItems :any[] =[];
+
+  private cartItemsList = new BehaviorSubject(this.cartItems);
+  latestCartItemsList: Observable<any[]> = this.cartItemsList.asObservable();
+
 
   constructor(private http: HttpClient) {}
 
@@ -17,17 +24,9 @@ export class CartService {
   addToCart(Bid: number): Observable<any> {
     return this.http.post(this.REST_API_URL+'Carts/', Bid).pipe(
       map((res: any) => {
-        console.log('hey');
         return res;
       })
     );
-    // let cartItem = this.cart.items.find(item => item.book.BId === book.BId);
-    // if (cartItem){
-    //   this.changeQuantity(book.BId, cartItem.quantity + 1);
-    //   return;
-    // }
-    // this.cart.items.push(new CartItem(book))
-    // console.log(this.cart);
   }
 
   removeFromCart(BId: number): Observable<any> {
@@ -36,25 +35,11 @@ export class CartService {
         return res;
       })
     );
-    //   this.cart.items.filter(item => item.book.BId != bookId);
-  }
+    }
 
-  // private INCREMENT_CART_QUANTITY_REST_API_URL = "https://localhost:44380/api/Carts/increment";
-  // incrementQuantity(Bid: number, count:number){
-  //   // let cartItem = this.cart.items.find(item => item.book.BId === bookId);
-  //   // console.log(cartItem);
-  //   // if (!cartItem) return;
-  //   // cartItem.quantity = quantity;
-  //   return this.http.put(this.INCREMENT_CART_QUANTITY_REST_API_URL,null,{params:{Bid:Bid,count:count}}).pipe(map((res:any)=>{
-  //     return res;
-  //   }));
-  // }
+
 
   changeQuantity(Bid: number, count: number) {
-    // let cartItem = this.cart.items.find(item => item.book.BId === bookId);
-    // console.log(cartItem);
-    // if (!cartItem) return;
-    // cartItem.quantity = quantity;
     return this.http
       .put(this.REST_API_URL+'Carts', null, {
         params: { Bid: Bid, count: count },
@@ -69,6 +54,9 @@ export class CartService {
   getCart(): Observable<any[]> {
     return this.http.get(this.REST_API_URL+'Carts/').pipe(
       map((res: any) => {
+        this.latestCartItemsList.pipe(take(1)).subscribe((items)=>{
+          this.cartItemsList.next(res);
+        });
         return res;
       })
     );
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { CartItem } from 'src/app/cart/model/cartItem.model';
+import { CartService } from 'src/app/cart/services/cart.service';
 import { SharedService } from '../../services/shared.service';
 
 @Component({
@@ -10,9 +12,13 @@ import { SharedService } from '../../services/shared.service';
 export class HeaderComponent implements OnInit {
   faShoppingCart = faShoppingCart;
   totalBookCount: number = 0;
-  constructor(private sharedService: SharedService) {} //inject shared Service and return from getCategory function to update totalBookCount
+  constructor(private sharedService: SharedService,
+    private cartService:CartService) {} //inject shared Service and return from getCategory function to update totalBookCount
 
   ngOnInit(): void {
-    this.totalBookCount = this.sharedService.getCartQuantity();
+   this.cartService.latestCartItemsList.subscribe((items)=>{
+      console.log(items);
+      this.totalBookCount = items.length;
+    });
   }
 }
